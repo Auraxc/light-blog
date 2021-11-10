fff44545441deb426523baf0683be0572acb9662
ab <abeats@live.com>
2021-11-10 22:50:42 +0800

    创建 index 蓝图，返回 index.html， 并且在 app.py 中注册蓝图

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
-#!/usr/bin/env python3
-import time
-
 from flask import Flask
+from routes.index import main as index_routes  # 这里我们从 routes 文件夹下的 index 引入 main, 并把它重命名为 index_routes，这样在注册的时候更直观
 
-
-def configured_app():
-    # web framework
-    # web application
-    # __main__
-    app = Flask(__name__)
-    # 设置 secret_key 来使用 flask 自带的 session
-    # 这个字符串随便你设置什么内容都可以
-    app.secret_key = "123"
-    # register_routes(app)
-    return app
-
-
-# 运行代码
 if __name__ == '__main__':
-    # app = configured_app()
     # debug 模式可以自动加载你对代码的变动, 所以不用重启程序
-    # host 参数指定为 '0.0.0.0' 可以让别的机器访问你的代码
-    # 自动 reload jinja
+    # host 参数指定为 '0.0.0.0' 可以让别的机器访问你的代码，本地调试时设为 localhost 即可
     app = Flask(__name__)
     app.secret_key = "123"
-
+    app.register_blueprint(index_routes)  # 注册路由
     config = dict(
         debug=True,
         host='localhost',
