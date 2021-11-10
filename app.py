6d37e976410380e9a7c6c259621f4d497a92df43
ab <abeats@live.com>
2021-11-10 23:27:43 +0800

    app.py 进一步抽象，独立 flask 初始化和路由注册函数

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
 from routes.index import main as index_routes  # 这里我们从 routes 文件夹下的 index 引入 main, 并把它重命名为 index_routes，这样在注册的时候更直观
 
+
+def configured_app():
+    # 初始化 flask
+    # 注册路由
+    app = Flask(__name__)
+    register_routes(app)
+    return app
+
+
+def register_routes(app):
+    # 注册路由
+    app.register_blueprint(index_routes)
+
+
 if __name__ == '__main__':
     # debug 模式可以自动加载你对代码的变动, 所以不用重启程序
     # host 参数指定为 '0.0.0.0' 可以让别的机器访问你的代码，本地调试时设为 localhost 即可
-    app = Flask(__name__)
-    app.secret_key = "123"
-    app.register_blueprint(index_routes)  # 注册路由
+    app = configured_app()
+
     config = dict(
         debug=True,
         host='localhost',
