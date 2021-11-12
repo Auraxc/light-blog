0e5d518bf3f896982af7052807b3f9949a273037
Auraxc <abeats@live.com>
2021-11-12 17:27:21 +0800

    + article 路由

---
+++ routes/article.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+from models.article import Article
+
+main = Blueprint('article', __name__)
+
+
+@main.route("/<article_id>")
+def index(article_id):
+    article = Article.one(id=article_id)
+    return render_template("article.html", article=article)
